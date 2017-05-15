fdf5bb7979d4795d743840ae3b5cd662040b4c9b
JArthurJohnston <joe.a.johnston@gmail.com>
2017-05-15 18:15:07 -0400

    Merged with master after having been out on a branch for WAY too long

---
+++ src/grimoire/ui/views/GrimoireDebugPanel.java
@@ -0,0 +1,11 @@
+package grimoire.ui.views;
+
+import javax.swing.*;
+
+/**
+ * Created by arthur on 15/05/17.
+ */
+public class GrimoireDebugPanel {
+    private JPanel panel1;
+    private JPanel videoPanel;
+}
