f1574c4aa4a85de4904f41c88dd6322ca381934b
JArthurJohnston <joe.a.johnston@gmail.com>
2018-02-15 07:25:20 -0500

    Configured gradle to add opencv to the library path when running tests.

--- GrimoireClient/src/test/java/com/paratussoftware/GrimoireTest.java
+++ GrimoireClient/src/test/java/com/paratussoftware/GrimoireTest.java
@@ -1,15 +1,12 @@
 package com.paratussoftware;
 
 import org.junit.Test;
-
-import java.util.Set;
-
 import static org.junit.Assert.assertEquals;
 
 public class GrimoireTest {
 
     @Test
-    public void testMain_SetsArgumentsToSettings() throws Exception {
+    public void testMain_SetsArgumentsToSettings() {
         Grimoire.main(new String[]{"999", "1024", "768"});
 
         assertEquals(999, Settings.PORT_NUMBER);
